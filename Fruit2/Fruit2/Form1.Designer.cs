0bd925ab6aa8088818ab80c6af1a617b9ce9d904
Chaed-Gluck <125264376+Changed-cc@users.noreply.github.com>
2025-01-04 19:42:57 +0800

    实验二：进程互斥与同步

---
+++ Fruit2/Fruit2/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Fruit2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.apple1 = new System.Windows.Forms.PictureBox();
+            this.orange1 = new System.Windows.Forms.PictureBox();
+            this.Plate = new System.Windows.Forms.PictureBox();
+            this.father = new System.Windows.Forms.PictureBox();
+            this.mother = new System.Windows.Forms.PictureBox();
+            this.son = new System.Windows.Forms.PictureBox();
+            this.daughter = new System.Windows.Forms.PictureBox();
+            this.father_hand = new System.Windows.Forms.PictureBox();
+            this.mother_hand = new System.Windows.Forms.PictureBox();
+            this.son_hand = new System.Windows.Forms.PictureBox();
+            this.daughter_hand = new System.Windows.Forms.PictureBox();
+            this.apple2 = new System.Windows.Forms.PictureBox();
+            this.orange2 = new System.Windows.Forms.PictureBox();
+            this.mama = new System.Windows.Forms.Label();
+            this.baba = new System.Windows.Forms.Label();
+            this.dau = new System.Windows.Forms.Label();
+            this.s = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.apple1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.father)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mother)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.son)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daughter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.father_hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mother_hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.son_hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daughter_hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apple2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // apple1
+            // 
+            this.apple1.Image = ((System.Drawing.Image)(resources.GetObject("apple1.Image")));
+            this.apple1.Location = new System.Drawing.Point(199, 83);
+            this.apple1.Name = "apple1";
+            this.apple1.Size = new System.Drawing.Size(50, 57);
+            this.apple1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.apple1.TabIndex = 33;
+            this.apple1.TabStop = false;
+            // 
+            // orange1
+            // 
+            this.orange1.Image = ((System.Drawing.Image)(resources.GetObject("orange1.Image")));
+            this.orange1.Location = new System.Drawing.Point(948, 60);
+            this.orange1.Name = "orange1";
+            this.orange1.Size = new System.Drawing.Size(50, 50);
+            this.orange1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.orange1.TabIndex = 32;
+            this.orange1.TabStop = false;
+            // 
+            // Plate
+            // 
+            this.Plate.Image = ((System.Drawing.Image)(resources.GetObject("Plate.Image")));
+            this.Plate.Location = new System.Drawing.Point(456, 165);
+            this.Plate.Name = "Plate";
+            this.Plate.Size = new System.Drawing.Size(329, 325);
+            this.Plate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Plate.TabIndex = 42;
+            this.Plate.TabStop = false;
+            // 
+            // father
+            // 
+            this.father.Image = ((System.Drawing.Image)(resources.GetObject("father.Image")));
+            this.father.Location = new System.Drawing.Point(12, 12);
+            this.father.Name = "father";
+            this.father.Size = new System.Drawing.Size(142, 150);
+            this.father.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.father.TabIndex = 38;
+            this.father.TabStop = false;
+            // 
+            // mother
+            // 
+            this.mother.Image = ((System.Drawing.Image)(resources.GetObject("mother.Image")));
+            this.mother.Location = new System.Drawing.Point(1060, 12);
+            this.mother.Name = "mother";
+            this.mother.Size = new System.Drawing.Size(103, 150);
+            this.mother.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mother.TabIndex = 39;
+            this.mother.TabStop = false;
+            // 
+            // son
+            // 
+            this.son.Image = ((System.Drawing.Image)(resources.GetObject("son.Image")));
+            this.son.Location = new System.Drawing.Point(1021, 490);
+            this.son.Name = "son";
+            this.son.Size = new System.Drawing.Size(142, 141);
+            this.son.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.son.TabIndex = 40;
+            this.son.TabStop = false;
+            // 
+            // daughter
+            // 
+            this.daughter.Image = ((System.Drawing.Image)(resources.GetObject("daughter.Image")));
+            this.daughter.Location = new System.Drawing.Point(12, 485);
+            this.daughter.Name = "daughter";
+            this.daughter.Size = new System.Drawing.Size(125, 146);
+            this.daughter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.daughter.TabIndex = 41;
+            this.daughter.TabStop = false;
+            // 
+            // father_hand
+            // 
+            this.father_hand.Image = ((System.Drawing.Image)(resources.GetObject("father_hand.Image")));
+            this.father_hand.Location = new System.Drawing.Point(187, 53);
+            this.father_hand.Name = "father_hand";
+            this.father_hand.Size = new System.Drawing.Size(74, 87);
+            this.father_hand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.father_hand.TabIndex = 34;
+            this.father_hand.TabStop = false;
+            // 
+            // mother_hand
+            // 
+            this.mother_hand.Image = ((System.Drawing.Image)(resources.GetObject("mother_hand.Image")));
+            this.mother_hand.Location = new System.Drawing.Point(957, 28);
+            this.mother_hand.Name = "mother_hand";
+            this.mother_hand.Size = new System.Drawing.Size(74, 82);
+            this.mother_hand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mother_hand.TabIndex = 35;
+            this.mother_hand.TabStop = false;
+            // 
+            // son_hand
+            // 
+            this.son_hand.Image = ((System.Drawing.Image)(resources.GetObject("son_hand.Image")));
+            this.son_hand.Location = new System.Drawing.Point(928, 485);
+            this.son_hand.Name = "son_hand";
+            this.son_hand.Size = new System.Drawing.Size(87, 97);
+            this.son_hand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.son_hand.TabIndex = 36;
+            this.son_hand.TabStop = false;
+            // 
+            // daughter_hand
+            // 
+            this.daughter_hand.Image = ((System.Drawing.Image)(resources.GetObject("daughter_hand.Image")));
+            this.daughter_hand.Location = new System.Drawing.Point(170, 485);
+            this.daughter_hand.Name = "daughter_hand";
+            this.daughter_hand.Size = new System.Drawing.Size(79, 97);
+            this.daughter_hand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.daughter_hand.TabIndex = 37;
+            this.daughter_hand.TabStop = false;
+            // 
+            // apple2
+            // 
+            this.apple2.Image = ((System.Drawing.Image)(resources.GetObject("apple2.Image")));
+            this.apple2.Location = new System.Drawing.Point(199, 83);
+            this.apple2.Name = "apple2";
+            this.apple2.Size = new System.Drawing.Size(50, 57);
+            this.apple2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.apple2.TabIndex = 45;
+            this.apple2.TabStop = false;
+            // 
+            // orange2
+            // 
+            this.orange2.Image = ((System.Drawing.Image)(resources.GetObject("orange2.Image")));
+            this.orange2.Location = new System.Drawing.Point(948, 60);
+            this.orange2.Name = "orange2";
+            this.orange2.Size = new System.Drawing.Size(50, 50);
+            this.orange2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.orange2.TabIndex = 46;
+            this.orange2.TabStop = false;
+            // 
+            // mama
+            // 
+            this.mama.AutoSize = true;
+            this.mama.Location = new System.Drawing.Point(1094, 175);
+            this.mama.Name = "mama";
+            this.mama.Size = new System.Drawing.Size(44, 18);
+            this.mama.TabIndex = 47;
+            this.mama.Text = "妈妈";
+            // 
+            // baba
+            // 
+            this.baba.AutoSize = true;
+            this.baba.Location = new System.Drawing.Point(56, 184);
+            this.baba.Name = "baba";
+            this.baba.Size = new System.Drawing.Size(44, 18);
+            this.baba.TabIndex = 48;
+            this.baba.Text = "爸爸";
+            // 
+            // dau
+            // 
+            this.dau.AutoSize = true;
+            this.dau.Location = new System.Drawing.Point(176, 613);
+            this.dau.Name = "dau";
+            this.dau.Size = new System.Drawing.Size(44, 18);
+            this.dau.TabIndex = 49;
+            this.dau.Text = "女儿";
+            // 
+            // s
+            // 
+            this.s.AutoSize = true;
+            this.s.Location = new System.Drawing.Point(954, 613);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(44, 18);
+            this.s.TabIndex = 50;
+            this.s.Text = "儿子";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 660);
+            this.Controls.Add(this.apple1);
+            this.Controls.Add(this.orange2);
+            this.Controls.Add(this.orange1);
+            this.Controls.Add(this.apple2);
+            this.Controls.Add(this.son_hand);
+            this.Controls.Add(this.daughter_hand);
+            this.Controls.Add(this.s);
+            this.Controls.Add(this.dau);
+            this.Controls.Add(this.baba);
+            this.Controls.Add(this.mama);
+            this.Controls.Add(this.father);
+            this.Controls.Add(this.mother);
+            this.Controls.Add(this.son);
+            this.Controls.Add(this.daughter);
+            this.Controls.Add(this.father_hand);
+            this.Controls.Add(this.mother_hand);
+            this.Controls.Add(this.Plate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Stop_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.apple1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.father)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mother)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.son)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daughter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.father_hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mother_hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.son_hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daughter_hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apple2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox apple1;
+        private System.Windows.Forms.PictureBox orange1;
+        private System.Windows.Forms.PictureBox Plate;
+        private System.Windows.Forms.PictureBox father;
+        private System.Windows.Forms.PictureBox mother;
+        private System.Windows.Forms.PictureBox son;
+        private System.Windows.Forms.PictureBox daughter;
+        private System.Windows.Forms.PictureBox father_hand;
+        private System.Windows.Forms.PictureBox mother_hand;
+        private System.Windows.Forms.PictureBox son_hand;
+        private System.Windows.Forms.PictureBox daughter_hand;
+        private System.Windows.Forms.PictureBox apple2;
+        private System.Windows.Forms.PictureBox orange2;
+        private System.Windows.Forms.Label mama;
+        private System.Windows.Forms.Label baba;
+        private System.Windows.Forms.Label dau;
+        private System.Windows.Forms.Label s;
+    }
+}
+
---
+++ Fruit2/Fruit2/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fruit2
+{
+    public partial class Form1 : Form
+    {
+        //启动标志
+        public static bool start_flag = false;
+        //互斥信号量，实现对盘子的互斥访问
+        public static Mutex mutex = new Mutex();
+        //同步信号量
+        public static Semaphore fruit_empty, fruit_full, apple_empty, apple_full, orange_empty, orange_full;
+        public static int apple_num = 0;
+        public static int orange_num = 0;
+        public static int fruit_num = 0;
+
+
+
+        int LplateX = 350;
+        int LplateY = 300;
+        int RplateX = 400;
+        int RplateY = 300;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            apple1.Visible = false;
+            orange1.Visible = false;
+            apple2.Visible = false;
+            orange2.Visible = false;
+            //设置最多允许有两个线程同时访问该信号量，且初始时该信号量的计数为2。
+            // 初始化信号量
+            fruit_empty = new Semaphore(2, 2);  // 盘子最多可容纳 2 个水果，初始化为可容纳 2 个
+            fruit_full = new Semaphore(0, 2);   // 盘子最开始为空，因此满的信号量初始为 0
+            apple_empty = new Semaphore(2, 2);  // 最开始没有苹果，信号量为 2，表示可放置
+            apple_full = new Semaphore(0, 2);   // 没有苹果时，表示已放置的苹果数为 0
+            orange_empty = new Semaphore(2, 2); // 最开始没有橘子，信号量为 2，表示可放置
+            orange_full = new Semaphore(0, 2);  // 没有橘子时，表示已放置的橘子数为 0
+
+            Task fatherTask = Task.Run(() => Father());
+            Task motherTask = Task.Run(() => Mother());
+            Task sonTask = Task.Run(() => Son());
+            Task daughterTask = Task.Run(() => Daughter());
+
+
+            
+
+        }
+
+        public void Father()
+        {
+            while (true)  // 生产者应一直尝试生产
+            {
+                // 盘子必须有空位才能放水果
+                fruit_empty.WaitOne();
+                fruit_empty.WaitOne();
+                // 苹果位置必须有空位
+                apple_empty.WaitOne();
+                apple_empty.WaitOne();
+                // 加锁仅用于对共享变量的操作
+                mutex.WaitOne();
+                // 执行手部动画，放苹果
+                F_hand_move(2);
+                // 增加苹果数量
+                apple_num+=2;
+                fruit_num+=2;
+                // 释放锁
+                mutex.ReleaseMutex();
+                apple_full.Release();
+                apple_full.Release();
+                fruit_full.Release();
+                fruit_full.Release();
+            }
+        }
+
+        public void Mother()
+        {
+            while (true)  // 生产者应一直尝试生产
+            {
+                fruit_empty.WaitOne();    // 盘子必须有空位才能放水果
+                orange_empty.WaitOne();   // 必须有空位放橘子
+                mutex.WaitOne();
+                M_hand_move(1);  // 执行手部动画，放橘子
+                orange_num++;
+                fruit_num++;
+                mutex.ReleaseMutex();     // 释放互斥锁
+                orange_full.Release();    // 增加一个橘子
+                fruit_full.Release();     // 盘子增加一个水果
+            }
+        }
+
+        public void Son()
+        {
+            while (true)  // 消费者应一直尝试消费
+            {
+                fruit_full.WaitOne();  // 等待盘子里有水果
+                orange_full.WaitOne(); // 等待有橘子
+                mutex.WaitOne();  // 加锁，确保对共享变量的安全操作
+                S_hand_move();  // 执行手部动画，取橘子
+                orange_num--;
+                fruit_num--;  // 减少盘子中水果的数量
+                mutex.ReleaseMutex();  // 释放互斥锁
+                orange_empty.Release(); // 通知生产者橘子位置空出来了
+                fruit_empty.Release();  // 通知盘子有空位了
+            }
+        }
+
+        public void Daughter()
+        {
+            while (true)  // 消费者应一直尝试消费
+            {
+                fruit_full.WaitOne();  // 等待盘子里有水果
+                apple_full.WaitOne();  // 等待有苹果
+                mutex.WaitOne();  // 加锁，确保对共享变量的安全操作
+                D_hand_move();  // 执行手部动画，取苹果
+                D_hand_move();
+                apple_num--;
+                fruit_num--;  // 减少盘子中水果的数量
+                mutex.ReleaseMutex();  // 释放互斥锁
+                apple_empty.Release(); // 通知生产者苹果位置空出来了
+                fruit_empty.Release(); // 通知盘子有空位了
+            }
+        }
+
+
+        // 爸爸的手的动画
+        public void F_hand_move(int num)
+        {
+            int x = 187, y = 53;  // 爸爸的手的初始位置
+            int n = 10;
+
+            
+            for (int i = 0; i < num; i++)
+            {
+                if (apple1.Visible == false && orange1.Visible == false)
+                {
+                    AnimateHandMovement(father_hand, apple1, x, y, 25, 20, n);
+                }
+                else if (apple2.Visible == false && orange2.Visible == false)
+                {
+                    AnimateHandMovement(father_hand, apple2, x, y, 30, 20, n);
+                }
+                else
+                {
+                    MessageBox.Show("盘子放满了！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+        }
+
+        // 妈妈的手的动画
+        public void M_hand_move(int num)
+        {
+            int x = 600, y = 53;
+            int n = 10;
+
+           
+            for (int i = 0; i < num; i++)
+            {
+                if (apple1.Visible == false && orange1.Visible == false)
+                {
+                    AnimateHandMovement(mother_hand, orange1, x, y, -30, 20, n);
+                }
+                else if (apple2.Visible == false && orange2.Visible == false)
+                {
+                    AnimateHandMovement(mother_hand, orange2, x, y, -25, 20, n);
+                }
+                else
+                {
+                    MessageBox.Show("盘子放满了！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+        }
+
+        //女儿的手的动画
+        public void D_hand_move()
+        {
+            int x = daughter_hand.Location.X, y = daughter_hand.Location.Y;
+            int n = 10;
+            if (apple1.Visible == true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    daughter_hand.Location = new Point(x, y);
+                    x += 21;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x -= 21;
+                    y += 8;
+                    apple1.Location = new Point(x, y);
+                    daughter_hand.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+                apple1.Visible = false;
+            }
+            else if (apple2.Visible == true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    daughter_hand.Location = new Point(x, y);
+                    x += 15;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x -= 15;
+                    y += 8;
+                    apple2.Location = new Point(x, y);
+                    daughter_hand.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+                apple2.Visible = false;
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    daughter_hand.Location = new Point(x, y);
+                    x += 12;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x -= 12;
+                    y += 8;
+                    daughter_hand.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        public void AnimateHandMovement(Control hand, Control fruit, int startX, int startY, int deltaX, int deltaY, int steps)
+        {
+            int x = startX;
+            int y = startY;
+
+            for (int i = 0; i < steps; i++)
+            {
+                hand.Location = new Point(x, y);
+                fruit.Visible = true;
+                fruit.Location = new Point(x, y);
+                x += deltaX;
+                y += deltaY;
+                Thread.Sleep(100);
+            }
+            for (int i = 0; i < steps; i++)
+            {
+                x -= deltaX;
+                y -= deltaY;
+                hand.Location = new Point(x, y);
+                Thread.Sleep(100);
+            }
+        }
+
+        //儿子的手的动画
+        public void S_hand_move()
+        {
+            int x = son_hand.Location.X, y = son_hand.Location.Y;
+            int n = 10;
+            if (orange1.Visible == true)    //盘子左边有橘子
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    son_hand.Location = new Point(x, y);
+                    x -= 15;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x += 15;
+                    y += 8;
+                    son_hand.Location = new Point(x, y);
+                    orange1.Visible = true;
+                    orange1.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+                orange1.Visible = false;
+            }
+            else if (orange2.Visible == true)   //盘子右边有橘子
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    son_hand.Location = new Point(x, y);
+                    x -= 21;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x += 21;
+                    y += 8;
+                    son_hand.Location = new Point(x, y);
+                    orange2.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+                orange2.Visible = false;
+            }
+            else if (orange1.Visible == false && orange2.Visible == false)    //盘中无橘子
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    son_hand.Location = new Point(x, y);
+                    x -= 12;
+                    y -= 8;
+                    Thread.Sleep(100);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    x += 12;
+                    y += 8;
+                    son_hand.Location = new Point(x, y);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+    }
+}
